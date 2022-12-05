661b709eff285ad3bd5ffe0966e90e0674d7369b
Filip Tuczyński <ftuczynski@gmail.com>
2022-12-05 23:51:46 +0100

    Add Day2 Part1

---
+++ Day02/ExtensionMethods.cs
@@ -0,0 +1,52 @@
+﻿namespace Day02;
+
+public static class ExtensionMethods
+{
+    public static readonly int WIN_POINTS = 6;
+    public static readonly int DRAW_POINTS = 3;
+    public static readonly int LOST_POINTS = 0;
+
+    public enum Hand
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissors = 3,
+    }
+
+    public static Hand MapHand(this char handCode) => handCode switch
+    {
+        'A' or 'X' => Hand.Rock,
+        'B' or 'Y' => Hand.Paper,
+        'C' or 'Z' => Hand.Scissors
+    };
+
+    public static int ScoreRound(Hand oponentHand, Hand myHand)
+    {
+        switch (oponentHand)
+        {
+            case Hand.Rock:
+                return ((int)myHand) + myHand switch
+                {
+                    Hand.Paper => WIN_POINTS,
+                    Hand.Rock => DRAW_POINTS,
+                    _ => LOST_POINTS
+                };
+            case Hand.Paper:
+                return ((int)myHand) + myHand switch
+                {
+                    Hand.Scissors => WIN_POINTS,
+                    Hand.Paper => DRAW_POINTS,
+                    _ => LOST_POINTS
+                };
+            case Hand.Scissors:
+                return ((int)myHand) + myHand switch
+                {
+                    Hand.Rock => WIN_POINTS,
+                    Hand.Scissors => DRAW_POINTS,
+                    _ => LOST_POINTS
+                };
+            default: return 0;
+        }
+    }
+}
+
---
+++ Day02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day02;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("------ Advent Of Code Day 2 ------");
+        var input = File.ReadAllLines("input.txt").Select(x => x.Split(" ").Select(x => Convert.ToChar(x)).ToArray()).ToArray();
+
+        //PART 1
+        Console.WriteLine("Part 1: " + SolvePartOne(input));
+
+        //PART 2
+        Console.WriteLine("Part 2: ");
+    }
+
+    static int SolvePartOne(char[][] input)
+    {
+        int score = 0;
+        foreach (var round in input)
+        {
+            score += ExtensionMethods.ScoreRound(round[0].MapHand(), round[1].MapHand());
+        }
+        return score;
+    }
+}
+

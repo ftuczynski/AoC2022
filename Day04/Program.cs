7e9439061d10a6fa5be09a2cf5f79fb7cc3d999d
Filip Tuczyński <ftuczynski@gmail.com>
2022-12-06 21:26:31 +0100

    Add Day4

---
+++ Day04/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Day04;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("------ Advent Of Code Day 4 ------");
+        var input = File.ReadAllLines("input.txt").Select(x => x.Split(',').Select(x => new Range(x)).ToArray()).ToArray();
+
+        //PART 1
+        Console.WriteLine("Part 1: " + SolvePartOne(input));
+
+        //PART 2
+        Console.WriteLine("Part 2: " + SolvePartTwo(input));
+    }
+    public static int SolvePartOne(Range[][] input)
+    {
+        return input.Count(ranges => ranges.RangesFullyOverlap());
+    }
+
+    public static int SolvePartTwo(Range[][] input)
+    {
+        return input.Count(ranges => ranges.RangesOverlap());
+    }
+}
+
+public static class ExtensionMethods
+{
+    public static bool RangesFullyOverlap(this Range[] ranges) =>
+        ranges[0].Start >= ranges[1].Start && ranges[0].End <= ranges[1].End ||
+        ranges[1].Start >= ranges[0].Start && ranges[1].End <= ranges[0].End;
+
+    public static bool RangesOverlap(this Range[] ranges) =>
+        !(ranges[0].End < ranges[1].Start || ranges[0].Start > ranges[1].End);
+}
---
+++ Day04/Range.cs
@@ -0,0 +1,13 @@
+﻿namespace Day04;
+public class Range
+{
+    public int Start { get; }
+    public int End { get; }
+
+    public Range(string range)
+    {
+        var rangeArray = range.Split('-');
+        Start = int.Parse(rangeArray[0]);
+        End = int.Parse(rangeArray[1]);
+    }
+}

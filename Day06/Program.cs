5ff505a5628ad66b6be9cf8715f510c5bbe70ea1
Filip Tuczyński <ftuczynski@gmail.com>
2022-12-08 17:43:32 +0100

    Add Day6

---
+++ Day06/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day06;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("------ Advent Of Code Day 5 ------");
+        var input = File.ReadAllText("input.txt");
+
+        //PART 1
+        Console.WriteLine("Part 1: " + FindMarker(input, 4));
+
+        //PART 2
+        Console.WriteLine("Part 2: " + FindMarker(input, 14));
+    }
+
+    public static int FindMarker(string input, int markerLenght)
+    {
+        Queue<char> marker = new();
+        input.Take(markerLenght-1).ToList().ForEach(x => marker.Enqueue(x));
+        for (int i = markerLenght-1; i < input.Count(); i++)
+        {
+            if (marker.Count == markerLenght) marker.Dequeue();
+            marker.Enqueue(input[i]);
+            if (marker.Distinct().Count() == markerLenght) return i + 1;
+        }
+        return 0;
+    }
+}
